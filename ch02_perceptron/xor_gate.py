5b6e09e90ed84e6dbeefe97f72bfe34bdc98392d
sangyeunpark <sangyeun003@gmail.com>
2024-02-08 15:36:00 +0900

    공부 시작

---
+++ ch02_perceptron/xor_gate.py
@@ -0,0 +1,13 @@
+from and_gate import AND
+from nand_gate import NAND
+from or_gate import OR
+
+def XOR(x1, x2):
+	s1 = NAND(x1, x2)
+	s2 = OR(x1, x2)
+	return AND(s1, s2)
+
+print(XOR(0, 0))
+print(XOR(1, 0))
+print(XOR(0, 1))
+print(XOR(1, 1))
